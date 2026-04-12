bd5d2644ec1888778dcbe09a3ec6e30be507d6e0
Shehan Uyanwatte <shehanuyanwatte2000@gmail.com>
2026-04-12 09:36:23 +0530

    Reduced epochs and added display of epoch error rate

--- ANN.py
+++ ANN.py
@@ -3,12 +3,12 @@ import numpy as np
 # --- STEP 1: GENERATE MILITARY INTELLIGENCE (DATA) ---
 # Features: [Speed (Mach), Radar Size (0-1), Altitude (k-ft)]
 # Hostiles: Fast, Small, High Altitude, Heat Signature
-hostiles = np.random.rand(20, 4) * [4.0, 0.9, 30, 10] + [0.5, 0.1, 5, 10]
+hostiles = np.random.rand(50, 4) * [4.0, 0.9, 30, 10] + [0.5, 0.1, 5, 10]
 # Friendlies: Slow, Large, Low Altitude
-friendlies = np.random.rand(20, 4) * [1.0, 0.5, 10, 1] + [0.5, 0.1, 5, 1]
+friendlies = np.random.rand(50, 4) * [1.0, 0.5, 10, 1] + [0.5, 0.1, 5, 1]
 
 X = np.vstack((hostiles, friendlies))
-y = np.array([1]*20 + [0]*20) # 1=Hostile, 0=Friendly
+y = np.array([1]*50 + [0]*50) # 1=Hostile, 0=Friendly
 
 # --- STEP 2: INITIALIZE THE NEURAL NETWORK ---
 weights = np.random.uniform(-1, 1, 4) 
@@ -16,7 +16,7 @@ bias = 0.0
 lr = 0.1
 totalerrors=0
 # --- STEP 3: TRAINING (THE LEARNING PHASE) ---
-for epoch in range(200):
+for epoch in range(10):
     for i in range(len(X)):
         # Linear Equation (Dot Product)
         z = np.dot(X[i], weights) + bias
@@ -28,9 +28,10 @@ for epoch in range(200):
             totalerrors+= 1
         weights += lr * error * X[i]
         bias += lr * error
-        
+    print(totalerrors) 
+    totalerrors=0
        
-print(totalerrors) 
+
 print("Targeting System Online. Weights Calibrated.")
 
 
